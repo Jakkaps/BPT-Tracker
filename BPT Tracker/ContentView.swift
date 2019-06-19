fc517c5ccbc41db4190934851617bd969b83eddf
Jens Amund Kristoffersen <me@jakkaps.com>
2019-06-19 09:52:49 +0200

    Initial Commit

---
+++ BPT Tracker/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  BPT Tracker
+//
+//  Created by Jens Amund on 19/06/2019.
+//  Copyright © 2019 Jakkaps. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
